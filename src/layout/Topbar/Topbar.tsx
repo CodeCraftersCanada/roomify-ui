68ac4a14ae1f699fe82157fce4b7520be4668cb4
Florece Ann Robles <annr.dev@gmail.com>
2023-12-06 08:39:03 -0500

    added topbar items

--- src/layout/Topbar/Topbar.tsx
+++ src/layout/Topbar/Topbar.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import "./Topbar.scss";
+import avatar from "../../assets/avatar.png";
+import { MdOutlineNotifications, MdOutlineSettings } from "react-icons/md";
 
 const Topbar = () => {
     return (
-        <div className="topbar-container">Topbar</div>
+        <div className="topbar-container">
+            <div className="notification-wrapper">
+                <MdOutlineSettings size={40} color="#626262"/>
+            </div>            
+            <div className="notification-wrapper">
+                <MdOutlineNotifications size={40} color="#626262"/>
+            </div>
+            <div className="avatar-wrapper">
+                <img src={avatar} alt="Avatar placeholder" />
+            </div>
+        </div>
     )
 }
 
