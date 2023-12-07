176666b76a9536b842db2d3c1c38a55ffb4d3df8
Florece Ann Robles <annr.dev@gmail.com>
2023-12-07 17:56:14 -0500

    added user detail route

--- src/enums/RoutePath.ts
+++ src/enums/RoutePath.ts
@@ -1,10 +1,11 @@
 export enum RoutePath {
     DASHBOARD = "/",
-    USER_VERIFICATION = "/user-verification",
+    USER_VERIFICATION = "/user",
     PROPERTY_VERIFICATION = "/property",
     PROPERTY_LISTING = "/property-listing",
     PROPERTY_AGREEMENT = "/property-agreement",
     PROPERTY_DETAIL = "/property/:id",
+    USER_DETAIL = "/user/:id",
     LOG_IN = "/login",
     LOG_OUT = "/logout",
     FORGOT_PASSWORD = "/forgot_password",
